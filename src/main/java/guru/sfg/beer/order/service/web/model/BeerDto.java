e9414500572811856d07092f6d49bd4a591740df
higaki-tatsuro <higaki8225@icloud.com>
2024-03-10 19:24:38 +0900

    2024/3/10 進捗

---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerDto.java
@@ -0,0 +1,51 @@
+package guru.sfg.beer.order.service.web.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto implements Serializable {
+
+    static final long serialVersionUID = -9041199281667911180L;
+
+    @Null
+    private UUID id;
+
+    @Null
+    private Integer version;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
+    @Null
+    private OffsetDateTime createDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
+    private String beerName;
+
+    @NonNull
+    private BeerStyleEnum beerStyle;
+
+    @NonNull
+    private String upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    @Positive
+    @NonNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/guru/sfg/beer/order/service/web/model/BeerStyleEnum.java
@@ -0,0 +1,10 @@
+package guru.sfg.beer.order.service.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PISLNER,
+    ALE,
+    STOUT,
+    GOSE,
+    IPA
+}
